5dc2066b8550768e55b8e4ef26a2c57e1df140f2
rodrigo roli <rodrigo.monteiro.roli@gmail.com>
2026-01-22 00:17:56 -0300

    feat: implement sqlc ctx + queries, add person read all route

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,58 @@
 package main
 
 import (
-	"log"
-	"os"
+	"context"
+	"encoding/json"
+	"fmt"
+	"go-movie-reservation/controller"
+	"go-movie-reservation/db"
+	"go-movie-reservation/movie_resevation"
+	"go-movie-reservation/repository"
+	"go-movie-reservation/usecase"
 
-	"github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+
+	server := gin.Default()
+
+	db, dbErr := db.ConnectDB()
+	if dbErr != nil {
+		fmt.Println(dbErr)
 	}
 
-	// Access environment variables
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	log.Println("DB Host:", dbHost)
-	log.Println("DB Port:", dbPort)
-	log.Println("DB User:", dbUser)
-	log.Println("DB Password:", dbPassword)
-	log.Println("DB Name:", dbName)
+	defer db.Close()
+
+	ctx := context.Background()
+	queries := movie_resevation.New(db)
+
+	// Repositories
+	PersonRepository := repository.NewPersonRepository(&ctx, queries)
+
+	// Usecases
+	PersonUseCase := usecase.NewPersonUseCase(PersonRepository)
+
+	// Controllers
+	PersonController := controller.NewPersonController(PersonUseCase)
+
+	// Routes
+	server.GET("/health", func(ginCtx *gin.Context) {
+		ginCtx.JSON(200, gin.H{
+			"message": "Everything is fine",
+		})
+	})
+
+	server.GET("/people", PersonController.GetPeople)
+
+	// Initialize Server
+	server.Run(":8080")
+}
+
+func Indent(v interface{}) string {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return fmt.Sprintf("%#v", v)
+	}
+	return string(b)
 }
