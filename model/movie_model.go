48e173e2825b71d5467c635f650dea803c801ed0
rodrigo roli <rodrigo.monteiro.roli@gmail.com>
2026-01-23 00:23:02 -0300

    feat: update Movie struct to include PosterExt and fix ReleaseDate type to time.Time

--- model/movie_model.go
+++ model/movie_model.go
@@ -1,12 +1,25 @@
 package model
 
+import "time"
+
 type Movie struct {
-	ID            string `json:"id"`
-	Title         string `json:"title"`
-	Description   string `json:"description"`
-	PosterImage   string `json:"posterImage"`
-	Minutes       int    `json:"minutes"`
-	ReleaseDate   string `json:"releaseDate"`
-	Language      string `json:"language"`
-	CountryOrigin string `json:"countryOrigin"`
+	ID            string    `json:"id"`
+	Title         string    `json:"title"`
+	Description   string    `json:"description"`
+	PosterImage   string    `json:"posterImage"`
+	PosterExt     string    `json:"posterExt"`
+	Minutes       int       `json:"minutes"`
+	ReleaseDate   time.Time `json:"releaseDate"`
+	Language      string    `json:"language"`
+	CountryOrigin string    `json:"countryOrigin"`
+}
+
+type Genre struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+}
+
+type MovieWithGenre struct {
+	Movie
+	Genres []string `json:"genres"`
 }
