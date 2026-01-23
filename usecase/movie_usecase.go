68d85ac2ed8335cf8fa47f8998f8900f9197eb6f
rodrigo roli <rodrigo.monteiro.roli@gmail.com>
2026-01-23 00:23:21 -0300

    feat: update MovieUseCase to handle MovieWithGenre and CRUD operations

--- usecase/movie_usecase.go
+++ usecase/movie_usecase.go
@@ -3,6 +3,8 @@ package usecase
 import (
 	"go-movie-reservation/model"
 	"go-movie-reservation/repository"
+
+	"github.com/google/uuid"
 )
 
 type MovieUseCase struct {
@@ -15,6 +17,22 @@ func NewMovieUseCase(repository repository.MovieRepository) MovieUseCase {
 	}
 }
 
-func (mu *MovieUseCase) GetMovies() ([]model.Movie, error) {
+func (mu *MovieUseCase) GetMovies() ([]model.MovieWithGenre, error) {
 	return mu.repository.GetMovies()
+}
+
+func (mu *MovieUseCase) GetMovie(id uuid.UUID) (model.MovieWithGenre, error) {
+	return mu.repository.GetMovie(id)
+}
+
+func (mu *MovieUseCase) CreateMovie(params model.CreateMovieInput) (model.MovieWithGenre, error) {
+	return mu.repository.CreateMovie(params)
+}
+
+func (mu *MovieUseCase) UpdateMovie(id uuid.UUID, params model.CreateMovieInput) (model.MovieWithGenre, error) {
+	return mu.repository.UpdateMovie(id, params)
+}
+
+func (mu *MovieUseCase) DeleteMovie(id uuid.UUID) error {
+	return mu.repository.DeleteMovie(id)
 }
