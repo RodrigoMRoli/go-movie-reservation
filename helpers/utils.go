ede46e1e2225cea3f581b9d7bcc66df1e60b5f5e
rodrigo roli <rodrigo.monteiro.roli@gmail.com>
2026-01-24 15:13:45 -0300

    feat: add utility functions for converting pointer types to SQL null types

---
+++ helpers/utils.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"database/sql"
+	"time"
+)
+
+func StringPointerToNullString(s *string) sql.NullString {
+	if s == nil {
+		return sql.NullString{Valid: false} // SQL ignora (COALESCE mantém o valor antigo)
+	}
+	return sql.NullString{String: *s, Valid: true} // Atualiza (mesmo se for "")
+}
+
+func IntPointerToNullInt32(i *int) sql.NullInt32 {
+	if i == nil {
+		return sql.NullInt32{Valid: false}
+	}
+	return sql.NullInt32{Int32: int32(*i), Valid: true}
+}
+
+func TimePointerToNullTime(t *time.Time) sql.NullTime {
+	if t == nil {
+		return sql.NullTime{Valid: false}
+	}
+	return sql.NullTime{Time: *t, Valid: true}
+}
