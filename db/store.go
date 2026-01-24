5af10cac36c3034b9f42b20bd6706e9f0ff5d649
rodrigo roli <rodrigo.monteiro.roli@gmail.com>
2026-01-24 15:45:08 -0300

    chores: remove useless comments

--- db/store.go
+++ db/store.go
@@ -5,17 +5,13 @@ import (
 	"database/sql"
 	"fmt"
 	"go-movie-reservation/movie_resevation"
-	// Importe o pacote gerado pelo sqlc
 )
 
-// Store define todas as funções de banco de dados disponíveis
-// Ela "herda" a interface Querier (gerada pelo sqlc) e adiciona ExecTx
 type Store interface {
 	movie_resevation.Querier
 	ExecTx(ctx context.Context, fn func(movie_resevation.Querier) error) error
 }
 
-// SQLStore é a implementação real
 type SQLStore struct {
 	*movie_resevation.Queries
 	db *sql.DB
@@ -28,7 +24,6 @@ func NewStore(db *sql.DB) Store {
 	}
 }
 
-// ExecTx executa uma função dentro de uma transação do banco
 func (store *SQLStore) ExecTx(ctx context.Context, fn func(movie_resevation.Querier) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
